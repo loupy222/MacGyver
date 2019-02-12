9efa246a6058b332c9b27929d7b9718e32e690c5
Nuno DA SILVA <loupy222@gmail.com>
2019-02-12 14:25:21 +0100

    suite graphics

--- graphics.py
+++ graphics.py
@@ -1,3 +1,6 @@
+import constants
+import labyrinth
+
 class main_dysplay:
     #Ouverture de la fenêtre Pygame (carré : largeur = hauteur)
     window = pygame.display.set_mode((constants.window_side_size, constants.window_side_size))
@@ -7,20 +10,23 @@ class main_dysplay:
     #Titre
     pygame.display.set_caption(constants.Title)
 
+g_board = main_dysplay()
+
 class Home_loop_display:
-    	#Home screen
+    #Home screen
 	home = pygame.image.load(constants.home_pic).convert()
 	window.blit(home, (0,0))
 
 	#Refresh screen
 	pygame.display.flip()
+home_board = Home_loop_display()
 
 class Game_loop_display:
 
     #Chargement du fond
-		back_img = pygame.image.load(constants.back_img).convert()
-		window.blit(back_img, (0,0))
-		labyrinth(window)		
-		#Création de Macgyver
+	back_img = pygame.image.load(constants.back_img).convert()
+	window.blit(back_img, (0,0))
+	labyrinth(window)
+    """labyrinth = labyrinth.my_lab"""
 
-    labyrinth = labyrinth.my_lab
+game_board = Game_loop_display ()
