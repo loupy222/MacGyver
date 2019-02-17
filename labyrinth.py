d58c8bde4f24f0b10389e0b1ac76742c11d879a2
Nuno DA SILVA <loupy222@gmail.com>
2019-02-17 18:26:14 +0100

    organisation labyrinth.py

--- labyrinth.py
+++ labyrinth.py
@@ -1,6 +1,7 @@
 import pprint
 import constants
 import random
+import items
 import pygame
 #import graphics
 
@@ -20,20 +21,6 @@ class Labyrinth:
         with open("draw_file",'r') as labyrinth:
             self.structure = [[letter for letter in line if letter != "\n"] for line in labyrinth]
 
-    """def show(self):
-        #This method is to display the game board
-        wall_picture = pygame.image.load(constants.wall).convert()
-        guardian_picture = pygame.image.load(constants.guardian).convert()
-        # To display the structure of labyrinth
-        for x, line in enumerate(self.structure):
-            for y, letter in enumerate(line):
-                position = position(x, y)
-                if self.structure[x][y] == "w":
-                    graphics.g_board.blit(wall_picture, (position.pixels_x, position.pixels_y))
-                elif self.structure[x][y] == "G":
-                    graphics.g_board.blit(guardian_picture,(position.pixels_x, position.pixels_y))
-    # take randomly a free space from the structure []"""
-
     def rand_free_tile(self):
         rand_line = random.randint(0, len(self.structure) -1)
         print (rand_line)
@@ -48,4 +35,17 @@ class Labyrinth:
 my_lab = Labyrinth()
 
 pprint.pprint(my_lab.structure)
-print(my_lab.rand_free_tile())
+
+    """def show(self):
+        #This method is to display the game board
+        wall_picture = pygame.image.load(constants.wall).convert()
+        guardian_picture = pygame.image.load(constants.guardian).convert()
+        # To display the structure of labyrinth
+        for x, line in enumerate(self.structure):
+            for y, letter in enumerate(line):
+                position = position(x, y)
+                if self.structure[x][y] == "w":
+                    graphics.g_board.blit(wall_picture, (position.pixels_x, position.pixels_y))
+                elif self.structure[x][y] == "G":
+                    graphics.g_board.blit(guardian_picture,(position.pixels_x, position.pixels_y))
+    # take randomly a free space from the structure []"""
