1154ed4db247b0893526de4a240fe065623e93a5
Nuno DA SILVA <loupy222@gmail.com>
2019-03-07 23:32:40 +0100

    modul g_events que j'utiliserait ou pas!

---
+++ g_events.py
@@ -0,0 +1,43 @@
+from pygame import event()
+
+
+class GameEvents:
+
+    def __init__(self, character):
+        self.character = character
+       
+    def choice (self):
+
+        for event in event.get():
+                
+            if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+                #choice = 0
+                game_loop = False
+                        
+            elif event.type == KEYDOWN:				
+                    
+                    if event.key == K_ENTER:
+                        choice = 0
+
+
+    def g_controls (self):
+            
+        for event in event.get():                
+                   
+            if event.type == QUIT:
+                game_loop = False
+                #choice = 0
+                
+            elif event.type == KEYDOWN:                       
+                if event.key == K_ESCAPE:
+                    game_loop = False
+                            
+
+                elif event.key == K_RIGHT:
+                    character.moove('right')
+                elif event.key == K_LEFT:
+                    character.moove('left')
+                elif event.key == K_UP:
+                    character.moove('up')
+                elif event.key == K_DOWN:
+                    character.moove('down')			
--- macgyver.py
+++ macgyver.py
@@ -35,11 +35,13 @@ lab.structure[poison.case_x][poison.case_y] = "P"
 lab.structure[Mac.case_x][Mac.case_y] = "M"
 pprint(lab.structure)
 
+controls = GameEvents(Mac)
+
 while game_loop:
 	print(Mac.back_pack)
 	lab.structure[Mac.case_x][Mac.case_y] = " "
-
-	user_answer = input("Quel direction voulez-vous prendre? (up, down, right, left)  ")
+	controls.g_controls()
+	"""user_answer = input("Quel direction voulez-vous prendre? (up, down, right, left)  ")
 
 	if user_answer == "right":
 		Mac.moove("right")
@@ -51,7 +53,7 @@ while game_loop:
 		Mac.moove("up")
 
 	elif user_answer == "down":
-		Mac.moove("down")
+		Mac.moove("down")"""
 
 	if lab.structure[Mac.case_x][Mac.case_y] == "G":
 		if len(Mac.back_pack) == 3:
