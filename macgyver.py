c6807b049e53b62ba497a80d6e0093ea8ed162ce
Nuno DA SILVA <loupy222@gmail.com>
2019-03-19 22:49:53 +0100

    terminé

--- macgyver.py
+++ macgyver.py
@@ -16,8 +16,3 @@ def game ():
 
     Home
     Game
-
-"""instancer ici pas sur loops"""
-"""jout display sur fentre pour backpack"""""" continuer les blit sur nouvelle fenetre backpack"""
-"""mettre tous les modules sur dossier avec un __init___.py"""
-
